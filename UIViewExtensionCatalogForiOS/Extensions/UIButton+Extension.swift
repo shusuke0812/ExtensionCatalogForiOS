a0da965f460a881779fd5ed35c53f29ee99f44ae
shusuke0812 <shu.ota0812@gmail.com>
2020-08-16 23:59:12 +0900

    [add]UIButtonのスタイル調整メソッド追加

---
+++ UIViewExtensionCatalogForiOS/Extensions/UIButton+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIButton+Extension.swift
+//  UIViewExtensionCatalogForiOS
+//
+//  Created by Shusuke Ota on 2020/8/16.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    /// ボタンのスタイルを設定する
+    /// - Parameters:
+    ///   - backgroundColor: 背景色（透明 = clear）
+    ///   - borderColor: 枠線
+    ///   - borderWidth: 枠線幅（推奨値 = 2.0）
+    ///   - cornerRadius: 角丸（推奨値 = 6.0）
+    func setButtonStyle(backgroundColor: CGColor, borderColor: CGColor, borderWidth: CGFloat = 2.0, cornerRadius: CGFloat = 6.0) {
+        self.layer.backgroundColor = backgroundColor
+        self.layer.borderColor = borderColor
+        self.layer.borderWidth = borderWidth
+        self.layer.cornerRadius = cornerRadius
+    }
+}
