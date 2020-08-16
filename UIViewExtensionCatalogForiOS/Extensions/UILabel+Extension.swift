0d862dde4801f563affa0f912919585eabf63299
shusuke0812 <shu.ota0812@gmail.com>
2020-08-17 00:14:03 +0900

    [add]UILabelの行間を変更するメソッドを追加

---
+++ UIViewExtensionCatalogForiOS/Extensions/UILabel+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UILabel+Extension.swift
+//  UIViewExtensionCatalogForiOS
+//
+//  Created by Shusuke Ota on 2020/8/17.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    /// ラベルの行間を変更する
+    /// - Parameters:
+    ///     - label: ラベルのテキスト
+    ///     - space: 行間
+    func changeLineSpacing(label: String, space: Float) -> NSMutableAttributedString {
+        let lineSpacingStyle = NSMutableParagraphStyle()
+        lineSpacingStyle.lineSpacing = CGFloat(space)
+        let lineSpacingAttribute = [NSAttributedString.Key.paragraphStyle: lineSpacingStyle]
+        return NSMutableAttributedString(string: label, attributes: lineSpacingAttribute)
+    }
+}
