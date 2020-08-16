c66e98c14f463c79f85a1b81773dbe394a15678e
shusuke0812 <shu.ota0812@gmail.com>
2020-08-17 00:15:45 +0900

    [add]UILabelにラベル高さを取得するメソッドを追加

--- UIViewExtensionCatalogForiOS/Extensions/UILabel+Extension.swift
+++ UIViewExtensionCatalogForiOS/Extensions/UILabel+Extension.swift
@@ -19,4 +19,10 @@ extension UILabel {
         let lineSpacingAttribute = [NSAttributedString.Key.paragraphStyle: lineSpacingStyle]
         return NSMutableAttributedString(string: label, attributes: lineSpacingAttribute)
     }
+    /// ラベルの高さを取得する
+    func getLabelFrameHeight() -> Int {
+        self.sizeToFit()
+        let labelFrameHeight: CGFloat = self.frame.height
+        return Int(labelFrameHeight)
+    }
 }
