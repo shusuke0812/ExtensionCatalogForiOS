92e44a2e809f2633bee748d7c22e04188e76b11a
shusuke0812 <shu.ota0812@gmail.com>
2020-08-17 00:10:11 +0900

    [add]UIImageViewの画像色を変えるメソッドを追加

---
+++ UIViewExtensionCatalogForiOS/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIImageView+Extension.swift
+//  UIViewExtensionCatalogForiOS
+//
+//  Created by Shusuke Ota on 2020/8/17.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    /// アイコンなどの画像色を変更する
+    /// - Parameters:
+    ///   - imageName: 画像の名前
+    ///   - tintColor: 画像の塗りつぶしの色
+    func changeImageColor(imageName: String, tintColor: UIColor) {
+        self.image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
+        self.tintColor = tintColor
+    }
+}
