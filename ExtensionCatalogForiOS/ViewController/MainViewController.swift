7443d529bf568863255d45521e6bc7857791a404
shusuke0812 <shu.ota0812@gmail.com>
2020-09-25 18:11:43 +0900

    [add]alertのテストコードを追記

--- ExtensionCatalogForiOS/ViewController/MainViewController.swift
+++ ExtensionCatalogForiOS/ViewController/MainViewController.swift
@@ -16,11 +16,16 @@ class MainViewController: UIViewController {
         self.setUI()
     }
     @IBAction func tapTestBtn(_ sender: Any) {
+        self.didTestMethod()
     }
 }
 
 extension MainViewController {
     private func setUI() {
         self.testBtnView.layer.cornerRadius = 10.0
+    }
+    // テスト用のコードをここに書く（使い捨て）
+    private func didTestMethod() {
+        UIAlertController.showAlertView(vc: self, title: "テスト表示", message: "こちらの内容でよろしいでしょうか？", preferredStyle: .alert, okBtnLabel: "OK", cancelBtnLabel: "キャンセル", completion: nil)
     }
 }
