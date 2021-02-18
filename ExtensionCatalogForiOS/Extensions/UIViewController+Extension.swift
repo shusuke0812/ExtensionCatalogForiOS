f838e0c79a1bc5b5aab32906bae9bb6aa7a3e987
shusuke0812 <shu.ota0812@gmail.com>
2021-02-18 12:49:41 +0900

    [add]UIViewControllerのextensionを追加
    ・キーボードを立ち上げた時にViewをその分だけ上にずらす（TextField等がキーボードに隠れることを防ぐため）
    ・キーボード以外をタップした時にキーボードが閉じる処理

---
+++ ExtensionCatalogForiOS/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,46 @@
+//
+//  UIViewController+Extension.swift
+//  ExtensionCatalogForiOS
+//
+//  Created by Shusuke Ota on 2021/2/18.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    /// キーボードを立ち上げた時にBaseViewをキーボードの高さ分だけ上にずらす通知をする
+    func addNotificationCenterForMovingViewWithKeyboardHeight() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            } else {
+                let suggestionHeight = self.view.frame.origin.y + keyboardSize.height
+                self.view.frame.origin.y -= suggestionHeight
+            }
+        }
+    }
+    @objc private func keyboardWillHide() {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    /// キーボードを立ち上げた時にBaseViewをキーボードの高さ分だけ上にずらす通知を外す
+    func removeNotificationCenterForMovingViewWithKeyboardHeight() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    /// キーボードを立ち上げた時にキーボード以外をタップしたらキーボードが閉じる
+    func setDissmissKeyboard() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dissmissKeyboard))
+        tapGestureRecognizer.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    @objc private func dissmissKeyboard() {
+        self.view.endEditing(true)
+    }
+}
